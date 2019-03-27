6eb9ac227a2dc5f07a06570ee7d515703479f68c
Eric Kaschalk <ekaschalk@gmail.com>
2019-03-27 15:59:14 -0700

    Add: LinkedList: Finally have intuiton for classic loop check

--- inv/linked_lists.py
+++ inv/linked_lists.py
@@ -84,6 +84,8 @@ class LinkedList(object):
         item = Node(val, _next=node._next)
         node._next = item
 
+        return item
+
     def push(self, val):
         item = Node(val)
 
@@ -92,14 +94,18 @@ class LinkedList(object):
 
         self.head = item
 
+        return item
+
     def append(self, val):
         item = Node(val)
 
         if self.head is None:
             self.head = item
-            return
+            return item
 
         self.tail._next = item
+
+        return item
 
     # *** Deletion
 
@@ -334,4 +340,97 @@ y.push(7)
 # * 6. Find the start of a linked list loop
 
 class LinkedList6(LinkedList):
-    pass
+    def find_loop(self):
+        slow, fast = self.head, self.head
+
+        while fast._next is not None:
+            slow, fast = slow._next, fast._next._next
+            if fast is None:
+                return None
+            if slow == fast:
+                break
+
+        slow = self.head
+
+        while slow != fast:
+            slow, fast = slow._next, fast._next
+            if fast is None:
+                return None
+
+        return slow
+
+x = LinkedList6()
+node0 = x.append(0)
+node1 = x.append(1)
+node2 = x.append(2)
+node3 = x.append(3)
+node4 = x.append(4)
+node5 = x.append(5)
+node5._next = node2
+
+# 5->2
+# 0 1 2 3 4 5  2 3 4 5  2 3 4 5
+# 0 2 4 2 4
+# 0 1  2  3 4 5
+# 4 5  2  3 4 5
+
+# 5->1
+# 0 1 2 3 4 5  1 2 3 4 5  1 2 3 4 5
+# 0 2 4 1 3 5
+# 0  1  2 3 4 5
+# 5  1  2 3 4 5
+
+# list(tz.take(24, zip(x, itertools.islice(x, 0, 1000, 2))))
+# list(tz.take(12, x))
+# x.find_loop()
+
+# https://medium.com/100-days-of-algorithms/day-62-linked-list-cycle-detection-a3f2db8cfaa0
+# ALG:
+# N = len(unique)
+# C = len(cyle)
+# T = N-C len(not-in-cycle)
+# k = steps in cycle (to meet up in the cycle)
+
+# when C=N
+# k == 2k (mod C)
+# k == 0  (mod C)
+# so k=C and C=N so k=N
+
+# When C != N
+# T+k == 2T + 2k (mod C)
+# T+k == 0 (mod C)
+# T+k == C (mod C)
+
+# not_in_cycle + steps_in_cycle == 2*not_in_cycle+2*steps_in_cycle MOD len(cycle)
+# not_cycle + steps_in_cycle == 0
+
+#       ------------
+# ------------------
+# - T - ---- C ----
+# ------ N ---------
+
+
+# 2k catches each other up, once we "close" T
+# how many steps does it take to close T?
+# T + k == 2T + 2k follows
+
+# AH Notice once T steps have been taken, then both pointers are in the loop
+
+# Now the k == 2k congruence is in effect
+
+#    from T == 2T (mod C) and k == 2k (mod C)
+
+# Where do the pointers meet inside of C? They meet T away from the start of C.
+# So if we move from start T and from where they meet, T away from C,
+# then they meet at the start of C!
+
+# Finalizing my understanding:
+# 1. When we move T spaces inside, the fast pointer has a 2T-T=T head-start
+# 2. When we detect a cycle, it will be T away from the finish.
+#    - Details on this step:
+#      if we move T spaces forward on both from where they meet
+#      then slow will be at start of C and fast will be T away from C
+#      ie. exactly where they began
+#      so working backwards, we would end up T away from finish
+#      when they both match
+# 3. So if we move T forward from start and where we stopped, we found cycle start
